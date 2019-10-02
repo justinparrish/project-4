d79c220cf812029164ca358b3d40104347010266
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-02 15:25:41 -0400

    migrating owner

--- car_maintenance_app/views.py
+++ car_maintenance_app/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
-from .serializers import CarSerializer
-from .models import Car
+from .serializers import CarSerializer, OwnerSerializer
+from .models import Car, Owner
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
