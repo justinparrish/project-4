efd223efd3ea8c03f2a610ee0c1378aa046fa636
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-02 15:20:33 -0400

    created Owner model

--- car_maintenance_app/models.py
+++ car_maintenance_app/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 year_choice = [(x,x) for x in range(1960,2019)]
 
+
+class Owner(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=345)
+
 class Car(models.Model):
     image_url = models.CharField(max_length=300)
     year = models.IntegerField(choices=year_choice)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
+    
+
     
 
 
