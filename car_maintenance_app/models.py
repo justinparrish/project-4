cc974e616cbb709803915fd9b9b931b4b43773d1
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-02 13:36:28 -0400

    finished model but not tested

--- car_maintenance_app/models.py
+++ car_maintenance_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Car(models.Model):
-
+    image = models.ImageField()
+    year = models.IntegerField()
+    make = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    
 
 
