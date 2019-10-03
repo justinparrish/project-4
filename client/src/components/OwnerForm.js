a1d8ec03b4d4ef7f8d9178a9d50795e91391c589
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-03 17:50:05 -0400

    added handle submit and input and successfully tested

--- client/src/components/OwnerForm.js
+++ client/src/components/OwnerForm.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
 
 export default class OwnerForm extends Component {
+    state = {
+        username: '',
+        email: ''
+    }
+
+    handleInput = (evnt) => {
+        let state = {...this.state}
+
+        state[evnt.target.name] = evnt.target.value
+
+        this.setState(state)
+    }
+
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        this.props.addNewOwner(this.state)
+    }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <label>Username</label>
-              <input type='text' name='username' placeholder='Username Here' />
+              <input type='text' name='username' placeholder='Username Here' onChange={this.handleInput} />
               <label>Email</label>
-              <input type='email' name='email' placeholder='Email Here' />
+              <input type='email' name='email' placeholder='Email Here' onChange={this.handleInput}/>
               <input type='submit' value='Add' />
             </form>
           )
