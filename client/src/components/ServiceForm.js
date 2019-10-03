892fd6ebf7615b402c0b48dc87bbc9ebd473dd9e
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-03 17:57:48 -0400

    added handle submit and input and successfully tested

--- client/src/components/ServiceForm.js
+++ client/src/components/ServiceForm.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react'
 
 export default class ServiceForm extends Component {
+    state = {
+        dealership: '',
+        location: '',
+        service: '',
+        mileage: 0,
+        price: 0,
+        date: '2019-01-01',
+        note: ''
+    }
+
+    handleInput = (evnt) => {
+        let state = {...this.state}
+
+        state[evnt.target.name] = evnt.target.value
+
+        this.setState( state )
+    }
+
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        this.props.addNewServiceHistory( this.state )
+    }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Dealership</label>
-                <input type='text' name='dealership' placeholder='Where was it serviced?' />
+                <input type='text' name='dealership' placeholder='Where was it serviced?' onChange={this.handleInput} />
                 <label>Location</label>
-                <input type='text' name='location' placeholder='Where was it located?' />
+                <input type='text' name='location' placeholder='Where was it located?' onChange={this.handleInput} />
                 <label>Service</label>
-                <input type='text' name='service' placeholder='What service did you have done?' />
+                <input type='text' name='service' placeholder='What service did you have done?' onChange={this.handleInput} />
                 <label>Mileage</label>
-                <input type='number' name='mileage' placeholder='Miles after service' />
+                <input type='number' name='mileage' placeholder='Miles after service' onChange={this.handleInput} />
                 <label>Price</label>
-                <input type='number' name='price' placeholder='How much it cost?' />
+                <input type='number' name='price' placeholder='How much it cost?' onChange={this.handleInput} />
                 <label>Date</label>
-                <input type='date' name='date' placeholder='When was it serviced?' />
+                <input type='date' name='date' placeholder='When was it serviced?' onChange={this.handleInput} />
                 <label>Note</label>
-                <input type='text' name='note' placeholder='Additional notes...' />
+                <input type='text' name='note' placeholder='Additional notes...' onChange={this.handleInput} />
                 <input type='submit' value='Add Service' />
             </form>
         )
