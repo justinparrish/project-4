c8d189bc613676511438b84097794213a6e68d51
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-03 10:56:42 -0400

    made migrations

--- car_maintenance_app/models.py
+++ car_maintenance_app/models.py
@@ -22,11 +22,9 @@ class Owner(models.Model):
     email = models.EmailField(max_length=345)
 
 class Car(models.Model):
-    image_url = models.CharField(max_length=300)
     year = models.IntegerField(choices=year_choice)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
-    mileage = models.BigIntegerField()
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='owners')
     
 class ServiceHistory(models.Model):
