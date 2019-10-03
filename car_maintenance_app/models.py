89ce5924067f96940237be92e551288635e85000
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-03 11:04:46 -0400

    added new field

--- car_maintenance_app/models.py
+++ car_maintenance_app/models.py
@@ -22,6 +22,7 @@ class Owner(models.Model):
     email = models.EmailField(max_length=345)
 
 class Car(models.Model):
+    image_url = models.CharField(max_length=300)
     year = models.IntegerField(choices=year_choice)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
