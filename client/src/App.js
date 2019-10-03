b8ef67d15d95708bda854799ad2d7abd8cea869f
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-03 12:33:32 -0400

    now can get all car information

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-const getCarNickname = (car) => (
+const carNickname = (car) => (
   <span>{car.nickname}</span>
 )
 
-const nickname = {nickname: 'name'}
+const carInfo = (car) => (
+  <p>{car.year} - {car.make} - {car.model}</p>
+)
+
+const carImage = (car) => (
+  <img src={car.image_url} />
+)
+
+
+const car = 
+  {
+    image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSd3O_uOKkMneXrRJZkaumguGiC-t8QkEgg8aww75TlRzox-Uc0'
+    ,nickname: 'name',
+    year: 2003,
+    make: 'chevy',
+    model: 'monte carlo'
+}
 
 const App = () => {
   return (
     <div>
     <h1>MotorBoard</h1>
-    {getCarNickname(nickname)}
+    {carNickname(car)}
+    {carInfo(car)}
+    {carImage(car)}
     </div>
   )
 }
